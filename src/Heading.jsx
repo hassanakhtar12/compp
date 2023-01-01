433bfcff231a0e62f1b14e54b2a606d8167e1442
hassan <hakhtar631@gmail.com>
2023-01-01 13:48:51 +0500

    changed

---
+++ src/Heading.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function React() {
+  return 
+  <h1>Hello Hassan Akhtar!!</h1>
+}
+
+export default Heading;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import './index.css'
+import './Heading'
+import Heading from "./Heading";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+  <React.Fragment>
+    <Heading></Heading>
+    <p>These are my favourite Netflix Series</p>
+    <ol>
+    <li>Stranger Things</li>
+    <li>Money Heist</li>
+    <li>Rise of the Ottoman</li>
+    <li>Vikings</li>
+    <li>Friends</li>
+    </ol>
+  </React.Fragment>
+  ,document.getElementById('root')
+)
